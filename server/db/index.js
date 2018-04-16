c8cc2e3be2b3961070b26b4a25ca72e72884b21a
Mightynumnums <deadcookies@gmail.com>
2018-04-16 18:33:35 -0400

    add model for spaceship

--- server/db/index.js
+++ server/db/index.js
@@ -4,3 +4,16 @@ const db = require('./db')
 require('./models')
 
 module.exports = db
+
+
+
+// const db = require('./db')
+
+// // register models
+// const {Spaceship, User } = require('./models')
+
+// module.exports = {
+//     db,
+//     User,
+//     Spaceship
+// }
