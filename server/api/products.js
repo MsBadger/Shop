6c546ca3363a327460115dca440f2e4f80263e39
Christine Ernst <christineernst@Christines-MacBook-Air.local>
2018-04-17 11:33:20 -0400

    all backend get routes are working (get all, get by id, get by category). closes #3.

--- server/api/products.js
+++ server/api/products.js
@@ -8,8 +8,18 @@ router.get('/', (req, res, next) => {
     .catch(next)
 })
 
+router.get('/category/:vesselType', (req, res, next) => {
+	console.log('THIS THIS THIS is req.params.vesselType', req.params.vesselType)
+	Spaceship.findAll({
+		where: { vesselType : req.params.vesselType.toString() }
+	})
+	.then(spaceships => res.json(spaceships))
+	.catch(next)
+})
+
 router.get('/:productId', (req, res, next) => {
 	Spaceship.findById(req.params.productId)
 	.then(spaceship => res.json(spaceship))
 	.catch(next)
 })
+
