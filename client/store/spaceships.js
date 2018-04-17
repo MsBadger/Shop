ff1968202db346e6193f775ef3cf1a0f92e8b4ce
Mightynumnums <deadcookies@gmail.com>
2018-04-17 13:53:34 -0400

    updated get by id, get all products, get by category

--- client/store/spaceships.js
+++ client/store/spaceships.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 /*** ACTION TYPES ***/
 const GET_SPACESHIPS = 'GET_SPACESHIPS';
+const GET_BY_CATEGORY = 'GET_SPACESHIP_CATEGORY'
 
 
 /*** ACTION CREATORS ***/
@@ -11,13 +12,32 @@ const getSpaceships = spaceships => {
         spaceships
     }
 }
+//get spaceships by category
+const getByCategory = spaceships => {
+    return {
+        type: GET_SPACESHIP_CATEGORY,
+        spaceships
+    }
+}
 
 /*** THUNK CREATORS ***/
 export const fetchSpaceships = () => dispatch => {
     axios.get('/api/products')
-        .then(res => dispatch(spaceships(res.data)))
+        .then(res => res.data)
+        .then(spaceships => {
+            dispatch(getSpaceships(spaceships))
+        })
         .catch(err => console.error('Fetching products/spaceships unsuccessful', err));
 };
+
+export const fetchByCategory = (category) => {
+    axios.get(`/api/products/${category}`)
+        .then(res => res.data)
+        .then(spaceships => {
+            dispatch(getByCategory(spaceships))
+        })
+        .catch(console.error('Fetching products/spaceships by category FAILED', err));
+}
 
 
 /*** REDUCER ***/
@@ -25,6 +45,9 @@ export default function (state = [], action) {
     switch (action.type) {
         case GET_SPACESHIPS:
             return action.getSpaceships;
+
+        case GET_SPACESHIP_CATEGORY:
+            return action.getByCategory;
 
         default:
             return state
