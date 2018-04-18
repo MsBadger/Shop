a2b8db1ac1305799af0b29b4d0c5a475ee662443
Christine Ernst <christineernst@Christines-MacBook-Air.local>
2018-04-18 13:08:46 -0400

    Capitalized navbar component file name. Also put all navbar links into a hamburger that still needs styling - added Bootstrap to index.html. Hamburger displays different links for administrators, guests and regular users.

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,29 +6,46 @@ import {logout} from '../store'
 
 const Navbar = ({ handleClick, isLoggedIn, isAdmin }) => (
   <div>
-  <Link to={'/'}>
-    <img src="/images/logo_sq.jpg" className="logo" />
-    <h1 className="header">Aquila Spaceships Store</h1>
-  </Link>    
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-      { isAdmin ? <Link to={'/spaceships/new'}>Add New Spaceship</Link> : null }
+    <nav className="navbar navbar-inverse navbar-static-top" role="navigation">
+      <Link to={'/'}>
+        <img src="/images/logo_sq.jpg" className="logo" />
+        <h1 className="header">Aquila Spaceships Store</h1>
+      </Link> 
+       <span className="nav-container">
+          <span className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+              </button>
+          </span>
+        {isLoggedIn ? (
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav">
+                  <li><Link to="/home">Account Home</Link></li>
+                  <li><a href="#" onClick={handleClick}>
+                    Logout
+                  </a></li>
+                  {isAdmin ? <li><Link to={'/spaceships/new'}>Add New Spaceship</Link></li> : null }
+              </ul>
+          </div>
+        ) : (
+          // <span class="dropdown">
+          //   {/* The navbar will show these links before you log in */}
+          //   <Link to="/login">Login</Link>
+          //   <Link to="/signup">Sign Up</Link>
+          // </span>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav">
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/signup">Sign Up</Link></li>
+                </ul>
+            </div>
+          
+        )}
+      </span>
     </nav>
-    <hr />
   </div>
 )
 
