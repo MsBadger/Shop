a228586e6bc38cfd11173d5565e4f68d96434b6d
Tania Santamaria <tsantamaria.r@gmail.com>
2018-04-19 18:39:36 -0400

    Delete Routes

--- server/db/models/lineItems.js
+++ server/db/models/lineItems.js
@@ -2,10 +2,16 @@ const Sequelize = require('sequelize');
 const db = require('../db.js');
 
 const LineItems = db.define('lineItems', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     quantity: {
         type: Sequelize.INTEGER,
         allowNull: false
     }
+
 })
 
 module.exports = LineItems;
